91706b725708b0154e5e8ade3e2632d5f7b08fc6
Carlos Godoy <carlosgodoy@pursuit.org>
2022-10-07 12:57:58 -0400

    added state code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import Child from './components/Child'
 import people from './people.json'
+import { useState } from 'react'
 
 function App() {
+  // useState takes initial value for variable as argument
+  // In the array we have TWO values:
+  // first value: the piece of state
+  // second value: the handler function to update that piece of state
+  const [mode, setMode] = useState('light')
 
-  console.log(people)
 
+  const changeMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+    } else {
+        setMode('light')
+    }
 
+    // mode === 'light' ? setMode('dark') : setMode('light')
+
+  }
+
+  // console.log(mode)
   return (
-    <div>
-      {
+    <div className={mode}>
+      {/* {
         // people.map(person => <div>{person.first_name}</div>)
 
         people.map((person, i) => { 
@@ -17,7 +33,12 @@ function App() {
             <Child first={person.first_name} last={person.last_name} key={person.id} />
           )
         })
-      }
+      } */}
+      <button onClick={changeMode} >Change to { mode === 'light' ? 'dark' : 'light' } mode</button>
+      <h1>{mode}</h1>
+
+
+
     </div>
   );
 }
